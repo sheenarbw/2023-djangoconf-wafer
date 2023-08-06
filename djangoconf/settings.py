4edae6ddc005a287054e43b2a9fbfb8f04fc295f
Sheena O'Connell <sheena.oconnell@gmail.com>
2023-08-06 07:50:22 +0200

    not much

--- djangoconf/settings.py
+++ djangoconf/settings.py
@@ -74,7 +74,7 @@ MEDIA_URL = "/media/"
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = os.environ.get("STATIC_URL", "/static/")
-
+# STATIC_ROOT = os.path.join(project_root, "static")
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
